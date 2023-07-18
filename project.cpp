7c99f1d7b5210243f9af7e925ac47309a716437f
Mohammad Saud <mohammadsaudattari@gmail.com>
2023-07-18 13:53:54 +0500

    project basic structure

---
+++ project.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+struct node{
+    string student_name, college_name,hostel_starts;
+    int hostel_fee, college_roll;
+    node* next;
+    node(){
+        student_name = college_name = hostel_starts ="";
+        hostel_fee = college_roll = 0;
+        next =  NULL;
+    }
+};
+
+class Hostel{
+    private:
+    node* start;
+    public:
+    Hostel(){
+        start = NULL;
+    }
+
+};
+
+int main(){
+    
+}
